93060b0c53833c90211ffeb927befcb939f837e4
CodBen <junes828@gmail.com>
2021-02-04 19:18:11 +0100

    Vérification JS + génération nb aléatoire

---
+++ javascript.js
@@ -0,0 +1,39 @@
+let input         = document.querySelector('#prix');
+let error         = document.querySelector('small');
+let formulaire    = document.querySelector('#formulaire');
+// Pointage de l'input, du submit et du texte erreur
+
+error.style.display = "none";
+// erreur caché
+
+let nombreAleatoire = Math.floor(Math.random() * 1001);
+let coups           = 0;
+let nombreChoisi;
+
+// Génération aléatoire d'un chiffre + initialisation variable de choix
+
+function verifier(nombre) {
+  
+    let instruction = document.createElement('div');
+  
+    if(nombre < nombreAleatoire) {
+      // C'est plus
+      instruction.textContent = "#" + coups + " ( " + nombre + " ) C'est plus !";
+      instruction.className = "instruction plus";
+    }
+    else if(nombre > nombreAleatoire) {
+      // C'est moins
+      instruction.textContent = "#" + coups + " ( " + nombre + " ) C'est moins !";
+      instruction.className = "instruction moins";
+    }
+    else {
+      // Félicitations 
+      instruction.textContent = "#" + coups + " ( " + nombre + " ) Félicitations vous avez trouvé le juste prix !";
+      instruction.className = "instruction fini";
+      input.disabled = true;
+    }
+
+//Vérification et comparaison.
+
+
+}
